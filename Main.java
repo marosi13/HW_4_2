39b064b3e6b7d4801e6560c4e702ea6c1e362812
marosi13 <67289775+marosi13@users.noreply.github.com>
2020-11-04 17:56:46 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BmiService service = new BmiService();
-        float weight = 80;
-        int growth = 176;
-        float index = service.calculate(weight, growth);
-        System.out.println(index);
-    }
-}
